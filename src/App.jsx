2db13f5f297817a68bc3bedf5bad3ba49660a70f
Андрей Едунов <opty@opty.ru>
2026-04-24 21:35:56 +0300

    downgrade to styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import styles from "./styles.module.css";
 export default function App() {
   return (
     <>
-      <h1 className={styles.header}>MiniStore</h1>
+      <h1 className="header">MiniStore</h1>
       <p>Первый экран</p>
     </>
   );
